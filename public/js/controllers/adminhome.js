5c8eaea004eee3d66fd5ef05f3c7581fbbf8c231
rajatarun <tarun17170@gmail.com>
2016-04-06 10:02:32 -0500

    fadsfs

--- public/js/controllers/adminhome.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').controller('adminHomeController',['$scope','userDetails','$location',function($scope,$location,userDetails){
-
-$scope.username=userDetails.getUsername();
-
-}])
